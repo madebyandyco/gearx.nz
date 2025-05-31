9d2472aa3ee958cd1eb5cf1cdb0cff16974d2ef6
madebyandyco <kern.that@gmail.com>
2025-05-31 14:36:34 +1000

    init push to git

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://gearx.nz/',
+  integrations: [tailwind()],
+  vite: {
+    server: {
+      fs: {
+        allow: ['..'],
+      },
+    },
+  },
+});
